479750c0a46fc87f4b3e15d2b8d0be5c9ed8337d
JogiYo <jo930921@gmail.com>
2021-10-08 19:50:22 +0900

    Comment Controller Complete
    Comment Controller의 코맨트 추가, 삭제, 변경 등의 기능을 추가

---
+++ FetchNoteServer/FetchNoteServerApplication.java
@@ -0,0 +1,13 @@
+package com.Team4.FetchNoteServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchNoteServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchNoteServerApplication.class, args);
+	}
+
+}
